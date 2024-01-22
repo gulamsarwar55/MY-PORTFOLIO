6602e1774b40b93ad45072a57be3c9560d68fafd
MD GULAM SARWAR <120262909+gulamsarwar55@users.noreply.github.com>
2024-01-22 20:03:40 +0530

    Add files via upload

---
+++ stl.js
@@ -0,0 +1,17 @@
+// let a = document.querySelector("about");
+// function gulam(){
+//     a.innerText = `hello my allaha`;
+
+// }
+
+
+function fun(){
+    document.querySelector("#demo").innerHTML = `Hello, This is MD GULAM SARWAR Student of Budge Budge Institute of Technology (KOLKATA)
+    , I am a software developer. `;
+}
+
+function fun1(){
+    document.querySelector('#demo1').innerHTML =`email id : gulaaam000@gmail.com `;
+    document.querySelector('#demo1').innerHTML =`contact no : 91 9608503255 `;
+}
+
